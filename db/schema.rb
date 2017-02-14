478302ad39ebfb7476e07826b622c873318a73ff
Courtland Messam <messamcourtland@gmail.com>
2017-02-14 12:06:12 -0500

    Changed Gemfile for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170213224013) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 20170213224013) do
     t.integer  "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_reviews_on_product_id"
+    t.index ["product_id"], name: "index_reviews_on_product_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +40,5 @@ ActiveRecord::Schema.define(version: 20170213224013) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "reviews", "products"
 end
