2931fd4e20360cdd71b0291fd5d43a841d55d03c
Courtland Messam <messamcourtland@gmail.com>
2017-02-14 00:44:10 -0500

    Alot of stuff mostly CSS and changes from ECommerce to Wyshlist

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212195249) do
+ActiveRecord::Schema.define(version: 20170213224013) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -27,6 +27,14 @@ ActiveRecord::Schema.define(version: 20170212195249) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_reviews_on_product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
